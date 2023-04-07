3ed676841a6f1987ab07decef96d3bb8639dbd33
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-08 03:52:57 +0530

    default value changes

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -12,12 +12,12 @@ LOG_FOLDER=${BASE_DIR}/models/training_logs
 LOG_FILE_NAME=${LOG_FOLDER}/log_training_${MODEL_NAME}.txt
 
 python -u ${BASE_DIR}/src/train.py \
-        --lr 1e-3 \
+        --lr 1e-2 \
         --weight_decay 1e-4 \
 		--trainer "sgd" \
 		--batch_size 64 \
         --num_workers 8 \
-        --num_epochs 6 \
+        --num_epochs 25 \
         --check_after 1 \
 		--checkpoint ${BASE_DIR}/models/model_checkpoint \
 		--data_folder ${BASE_DIR}/data \
