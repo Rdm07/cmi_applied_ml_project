ee8ba75b246f3aad28f602a2d038ebfd582049dd
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-10 07:28:31 +0530

    changed naem

--- scripts/run_training_new.sh
+++ scripts/run_training_new.sh
@@ -11,7 +11,7 @@ MODEL_NAME="model1"
 LOG_FOLDER=${BASE_DIR}/models/training_logs
 LOG_FILE_NAME=${LOG_FOLDER}/log_training_${MODEL_NAME}.txt
 
-python -u ${BASE_DIR}/src/train_cuda.py \
+python -u ${BASE_DIR}/src/train_new_cuda.py \
         --lr 1e-2 \
         --weight_decay 1e-4 \
 		--trainer "sgd" \
