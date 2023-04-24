96279e40c3bf58f2654d86a79caf3edae7c59d2b
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-25 02:05:05 +0530

    separated func for dataloader and single img pred

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,26 @@
+import torchvision, pickle
+import torch.nn.functional as F
+
+from torchvision import transforms
+
 # Local Imports
 from utils import *
 
+os.chdir(os.path.dirname(__file__))
+
+with open('../data/label_dict', 'rb') as handle:
+    label_dict = pickle.load(handle)
+
+# Data Transforms
+data_transforms = {
+	'val': transforms.Compose([
+		transforms.ToTensor(),
+		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+	]),
+}
+
 # Function to generate predictions for each model on the test dataset
-def run_test(model, test_loader, device):
+def run_test_loader(model, test_loader, device):
 	model.eval()
 	pred_labels = []
 	orig_labels = []
@@ -16,7 +34,8 @@ def run_test(model, test_loader, device):
 			inputs = inputs.to(torch.float).to(device)
 			targets = targets.to(torch.long).to(device)
 			output = model(inputs)
-			if type(output) == tuple:
+			# For InceptionNet_v3
+			if type(output) == torchvision.models.inception.InceptionOutputs:
 				output,_ = output
 			
 			_, preds = torch.max(output.data, 1)
@@ -31,3 +50,27 @@ def run_test(model, test_loader, device):
 	pred_labels = np.array(pred_labels).reshape(-1,1)
 
 	return orig_labels, pred_labels
+
+def classify_image(model, image_path, device, label_dict=label_dict):
+	model.eval()
+
+	img = Image.open(image_path).convert('RGB')
+	png = img.resize((224,224))
+	png = data_transforms['val'](png).unsqueeze(0)
+
+	inputs = png.to(torch.float).to(device)
+	output = model(inputs)
+
+	# For InceptionNet_v3
+	if type(output) == torchvision.models.inception.InceptionOutputs:
+		output,_ = output
+	
+	pred_prob = F.softmax(output.data, 1)
+	top_p, top_class = pred_prob.topk(1, dim = 1)
+
+	pred_class = int(torch.squeeze(top_class).data.cpu())
+	pred_prob = round(float(torch.squeeze(top_p).data.cpu()), 5)*100
+
+	pred_class_lab = label_dict[pred_class]
+
+	return pred_class_lab, pred_prob
